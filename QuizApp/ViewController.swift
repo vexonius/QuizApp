685c6533c4724a1b0c1f9cddb07e187c7b037831
Tino Emer <tinoemer@Tinos-MacBook-Pro.local>
2022-07-12 10:52:09 +0200

    Created empty project without storyboards

---
+++ QuizApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Tino Emer on 11.07.2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .purple
+    }
+
+}
+
