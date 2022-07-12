fd4b2f9ed4bcca281bf2fe5d1e9a381d814636e7
vexonius <tinoet@ymail.com>
2022-07-12 14:32:30 +0200

    Initial project setup (#1)
    * Created empty project without storyboards
    * Cleaned up codebase with team's code style guidelines
    * Removed unused delegate methods
    * Removed newlines before imports

---
+++ QuizApp/SceneDelegate.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        window = UIWindow(windowScene: windowScene)
+        
+        guard let window = window else { return }
+
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+    }
+
+}
+
