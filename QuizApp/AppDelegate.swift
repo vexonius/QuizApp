e6fb532ed5e99fbda10b46a432f37bcf93e1a0ce
Tino Emer <tinoemer@Tinos-MacBook-Pro.local>
2022-07-12 14:05:34 +0200

    Removed unused delegate methods

--- QuizApp/AppDelegate.swift
+++ QuizApp/AppDelegate.swift
@@ -19,9 +19,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-
-    }
-
 }
 
