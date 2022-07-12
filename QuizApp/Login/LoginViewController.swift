edb48efae24cad3585de9243adaa289444e4e9b5
Tino Emer <tinoemer@Tinos-MacBook-Pro.local>
2022-07-12 15:34:01 +0200

    Restructured app hierarchy

--- QuizApp/Login/LoginViewController.swift
+++ QuizApp/Login/LoginViewController.swift
@@ -1,11 +1,10 @@
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .purple
     }
 
 }
