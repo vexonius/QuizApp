4aa54218fe142f462d454888a7a1dbea47443dc1
Tino Emer <tinoemer@Tinos-MacBook-Pro.local>
2022-07-29 12:58:28 +0200

    Created account network models & endpoints

---
+++ QuizApp/Client/Account/Models/AccountDetailsNetworkModel.swift
@@ -0,0 +1,8 @@
+struct AccountDetailsNetworkModel {
+
+    let id: Int
+    let usename: String?
+    let name: String
+    let email: String
+
+}
---
+++ QuizApp/Client/Account/Models/UsernameUpdateRequestNetworkModel.swift
@@ -0,0 +1,5 @@
+struct UsernameUpdateRequestNetworkModel {
+
+    let username: String
+
+}
