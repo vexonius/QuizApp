f182c5831302313837b9f2f44e7d4e5e3bd61449
Tino Emer <tinoemer@Tinos-MacBook-Pro.local>
2022-08-01 14:39:57 +0200

    Added Reachability lib for observing network status through combine notifications

--- QuizApp/Presentation/HomeViewController.swift
+++ QuizApp/Presentation/HomeViewController.swift
@@ -1,10 +1,47 @@
+import Combine
 import UIKit
+import CombineReachability
+import Reachability
 
 class HomeViewController: BaseViewController {
+
+    private var cancellables = Set<AnyCancellable>()
+    private var reachability: Reachability?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .blue
+        reachability = try? Reachability()
+
+        bindReachability()
     }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        startObservingNetwork()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        stopObservingNetwork()
+    }
+
+    private func stopObservingNetwork() {
+        reachability?.stopNotifier()
+    }
+
+    private func startObservingNetwork() {
+        try? reachability?.startNotifier()
+    }
+
+    private func bindReachability() {
+        reachability?.reachabilityChanged
+            .sink { [weak self] reachability in
+                let connectionStatus = reachability.connection
+            }
+            .store(in: &cancellables)
+    }
+
 }
