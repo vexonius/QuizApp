80e951a1a93f59e8db77c965a8269be0e52eeb0e
Tino Emer <tinoemer@tinos-mbp.lan>
2022-08-12 15:07:15 +0200

    Added Ranking and HeaderView cells for leaderboard tableview

---
+++ QuizApp/Presentation/Leaderboard/RankingCell.swift
@@ -0,0 +1,122 @@
+import UIKit
+
+class RankingCell: UITableViewCell {
+
+    static let reuseIdentifier = String(describing: RankingCell.self)
+
+    private struct CustomConstants {
+        static let textNumberOfLines = 0
+    }
+
+    private var containerView: UIView!
+    private var rankLabel: UILabel!
+    private var userLabel: UILabel!
+    private var pointsLabel: UILabel!
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        selectionStyle = .none
+        backgroundColor = .clear
+
+        createViews()
+        styleViews()
+        defineLayoutForViews()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func createContainerView() {
+        containerView = UIView()
+        addSubview(containerView)
+    }
+
+    private func createUserLabel() {
+        userLabel = UILabel()
+        containerView.addSubview(userLabel)
+    }
+
+    private func createRankLabel() {
+        rankLabel = UILabel()
+        containerView.addSubview(rankLabel)
+    }
+
+    private func createPointsLabel() {
+        pointsLabel = UILabel()
+        containerView.addSubview(pointsLabel)
+    }
+
+}
+
+extension RankingCell: ConstructViewsProtocol {
+
+    func createViews() {
+        createContainerView()
+        createRankLabel()
+        createUserLabel()
+        createPointsLabel()
+    }
+
+    func styleViews() {
+        rankLabel.textAlignment = .left
+        rankLabel.textColor = .white
+        rankLabel.font = .sourceSansPro(
+            ofSize: DesignConstants.FontSize.title.cgFloat,
+            ofWeight: SourceSansProWeight.bold)
+
+        userLabel.textAlignment = .left
+        userLabel.textColor = .white
+        userLabel.lineBreakMode = .byTruncatingTail
+        userLabel.numberOfLines = CustomConstants.textNumberOfLines
+        userLabel.font = .sourceSansPro(
+            ofSize: DesignConstants.FontSize.subtitle.cgFloat,
+            ofWeight: SourceSansProWeight.semibold)
+
+        pointsLabel.textAlignment = .left
+        pointsLabel.textColor = .white
+        pointsLabel.lineBreakMode = .byTruncatingTail
+        pointsLabel.numberOfLines = CustomConstants.textNumberOfLines
+        pointsLabel.font = .sourceSansPro(
+            ofSize: DesignConstants.FontSize.title.cgFloat,
+            ofWeight: SourceSansProWeight.bold)
+    }
+
+    func defineLayoutForViews() {
+        containerView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+
+        rankLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(DesignConstants.Insets.contentInset)
+            make.top.bottom.equalToSuperview()
+            make.centerY.equalToSuperview()
+        }
+
+        userLabel.snp.makeConstraints { make in
+            make.leading.equalTo(rankLabel.snp.trailing).offset(DesignConstants.Insets.contentInset)
+            make.top.bottom.equalToSuperview()
+            make.centerY.equalToSuperview()
+        }
+
+        pointsLabel.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().inset(DesignConstants.Insets.contentInset)
+            make.top.bottom.equalToSuperview()
+            make.centerY.equalToSuperview()
+            make.leading.equalTo(userLabel.snp.trailing).offset(DesignConstants.Insets.contentInset)
+        }
+
+    }
+
+}
+
+extension RankingCell {
+
+    func bind(with model: UserRankingModel) {
+        rankLabel.text = "1."
+        userLabel.text = model.name
+        pointsLabel.text = String(model.points)
+    }
+
+}
