9068b0afea78ca47e2a121f6b7eaf35d34b8e888
Tino Emer <tinoemer@tinos-mbp.lan>
2022-08-12 15:09:59 +0200

    QuizLeaderboardViewModel class implemented

---
+++ QuizApp/Presentation/Leaderboard/QuizLeaderboardViewModel.swift
@@ -0,0 +1,35 @@
+import Combine
+
+class QuizLeaderboardViewModel {
+
+    @Published var userRankings: [UserRankingModel] = []
+
+    private let quizUseCase: QuizUseCaseProtocol
+    private let coordinator: QuizCoordinatorProtocol
+
+    init(quizId: Int, quizUseCase: QuizUseCaseProtocol, coordinator: QuizCoordinatorProtocol) {
+        self.quizUseCase = quizUseCase
+        self.coordinator = coordinator
+
+        getQuizLeaderBoard(for: quizId)
+    }
+
+    func onCloseItemTap() {
+        coordinator.dismiss()
+    }
+
+    private func getQuizLeaderBoard(for quizId: Int) {
+        Task {
+            do {
+                let rankingsModel = try await quizUseCase.getLeaderboard(for: quizId)
+
+                await MainActor.run {
+                    self.userRankings = rankingsModel
+                }
+            } catch {
+                debugPrint(error)
+            }
+        }
+    }
+
+}
