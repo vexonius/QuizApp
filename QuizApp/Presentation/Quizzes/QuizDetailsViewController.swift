8b19b1e85cc9ace09701716fefee715a6b264487
Tino Emer <tinoemer@tinos-mbp.lan>
2022-08-18 23:05:44 +0200

    Removed back item title on quiz answering screen

--- QuizApp/Presentation/Quizzes/QuizDetailsViewController.swift
+++ QuizApp/Presentation/Quizzes/QuizDetailsViewController.swift
@@ -77,6 +77,7 @@ class QuizDetailsViewController: BaseViewController {
 
     private func styleNavigationBar() {
         title = LocalizedStrings.appName.localizedString
+        navigationItem.backButtonTitle = ""
     }
 }
 
