301d4e3940e0143edf72f4d39990d1dcea920314
Tino Emer <tinoemer@tinos-mbp.lan>
2022-08-29 11:45:01 +0200

    Created SwiftUI RoundedButton & RoundedTextField counterparts structs

---
+++ QuizApp/Presentation/Common/SwiftUI/RoundedButton.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct RoundedButton: ViewModifier {
+
+    func body(content: Content) -> some View {
+        content
+            .padding([.vertical], 14)
+            .background(.white)
+            .clipShape(Capsule())
+            .frame(maxWidth: .infinity)
+    }
+
+}
---
+++ QuizApp/Presentation/Common/SwiftUI/RoundedTextInput.swift
@@ -0,0 +1,17 @@
+import Combine
+import SwiftUI
+
+struct RoundedTextInput: ViewModifier {
+
+    func body(content: Content) -> some View {
+        content
+            .font(.sourceSansPro(size: DesignConstants.FontSize.regular.cgFloat, weight: .semibold))
+            .foregroundColor(.white)
+            .accentColor(.white)
+            .padding([.vertical], 14)
+            .padding([.horizontal], 24)
+            .background(.white.opacity(0.3))
+            .cornerRadius(.infinity)
+    }
+
+}
