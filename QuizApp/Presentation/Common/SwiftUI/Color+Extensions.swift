809d0ab8d03dd2fad0d801c7a8b26557136f5d42
Tino Emer <tinoemer@Tinos-MacBook-Pro.local>
2022-08-30 15:06:17 +0200

    Expanded SwiftUI colors

--- QuizApp/Presentation/Common/SwiftUI/Color+Extensions.swift
+++ QuizApp/Presentation/Common/SwiftUI/Color+Extensions.swift
@@ -5,5 +5,7 @@ extension Color {
     static let darkerPurple = Color("darker-purple")
     static let lightPurple = Color("lighter-purple")
     static let accentRed = Color("quiz-red")
+    static let accentYellow = Color("quiz-yellow")
+    static let accentBlue = Color("quiz-blue")
 
 }
