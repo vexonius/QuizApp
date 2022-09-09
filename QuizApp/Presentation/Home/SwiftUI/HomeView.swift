3bc52b00442472d06f9b0f12efceab96e537b585
Tino Emer <tinoemer@Tinos-MacBook-Pro.local>
2022-09-09 13:44:53 +0200

    Created EmptyListPlaceholderViewModifier

--- QuizApp/Presentation/Home/SwiftUI/HomeView.swift
+++ QuizApp/Presentation/Home/SwiftUI/HomeView.swift
@@ -16,23 +16,15 @@ struct HomeView: View {
 
     var body: some View {
         List {
-            if viewModel.filteredQuizzes.isEmpty {
-                ForEach(0..<numberOfPlaceholderItems) { _ in
-                    QuizItemPlaceholderView()
-                        .listRowInsets(EdgeInsets(top: .zero, leading: .zero, bottom: .zero, trailing: .zero))
-                        .listRowBackground(Color.clear)
-                        .transition(.opacity)
-                }
-            } else {
-                ForEach(viewModel.filteredQuizzes, id: \.id) { quiz in
-                    QuizItemView(quiz: quiz)
-                        .frame(maxWidth: .infinity)
-                        .listRowInsets(EdgeInsets(top: .zero, leading: .zero, bottom: .zero, trailing: .zero))
-                        .listRowBackground(Color.clear)
-                        .transition(.opacity)
-                }
+            ForEach(viewModel.filteredQuizzes, id: \.id) { quiz in
+                QuizItemView(quiz: quiz)
+                    .frame(maxWidth: .infinity)
+                    .listRowInsets(EdgeInsets(top: .zero, leading: .zero, bottom: .zero, trailing: .zero))
+                    .listRowBackground(Color.clear)
+                    .transition(.opacity)
             }
         }
+        .emptyListPlaceholder(view: AnyView(QuizItemPlaceholderView()), visible: viewModel.filteredQuizzes.isEmpty)
         .modifier(ScrollViewBackgroundModifier())
         .brandStyleBackground()
         .onAppear(perform: viewModel.observeNetworkChanges)
