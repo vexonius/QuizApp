3afbdd0b7c51d1bd0263e6581a9abeeca780d0dd
Tino Emer <tino.emer@five.agency>
2022-09-29 16:23:47 +0200

    Added Xcode cloud prebuild swiftlint script

---
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+brew tap vexonius/five-swiftlint
+brew install five-swiftlint
+five-swiftlint lint --strict
+result=$?
+if [ "$result" = "2" ] || [ "$result" = "3" ]
+then
+    exit -1
+else
+    exit 0
+fi
